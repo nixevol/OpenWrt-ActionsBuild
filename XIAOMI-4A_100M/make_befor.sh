83011b7e3132b70138c1bfa7d4ebc66eb057dae7
guaotiantang <35430449+guaotiantang@users.noreply.github.com>
2022-01-20 02:04:59 +0800

    Add files via upload

---
+++ XIAOMI-4A_100M/make_befor.sh
@@ -0,0 +1,6 @@
+#befor make
+
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
