1692530b9311d292c66105a02714704d70b9381a
guaotiantang <35430449+guaotiantang@users.noreply.github.com>
2022-01-25 00:27:39 +0800

    Update make_befor.sh

--- XIAOMI-4A_GIGABit/make_befor.sh
+++ XIAOMI-4A_GIGABit/make_befor.sh
@@ -4,6 +4,6 @@
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 sed -i 's/192.168.1.1/192.168.32.1/g' package/base-files/files/bin/config_generate
-cd package/
+cd ./package/
 git clone https://github.com/jerrykuku/luci-theme-argon.git
 cd ../
