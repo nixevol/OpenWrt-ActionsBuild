7b4cb6de8e3be06a7b5fb24cfbd2f1e830dff816
guaotiantang <35430449+guaotiantang@users.noreply.github.com>
2022-01-30 03:47:07 +0800

    Update openwrt_patch.sh

--- XIAOMI-4A_GIGABit/openwrt_patch.sh
+++ XIAOMI-4A_GIGABit/openwrt_patch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "openwrt patch"
 sed -i 's/192.168.1.1/192.168.32.1/g' package/base-files/files/bin/config_generate
 cd package/
 git clone https://github.com/jerrykuku/luci-theme-argon.git
