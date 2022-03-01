bbb6a4db972875ef559a739589b4a410df74e004
guaotiantang <35430449+guaotiantang@users.noreply.github.com>
2022-03-01 22:56:13 +0800

    Update make_befor.sh

--- XIAOMI-4A_100M/make_befor.sh
+++ XIAOMI-4A_100M/make_befor.sh
@@ -1,6 +1,19 @@
-#befor make
+#!/bin/bash
 
-#rm -rf package/lean/luci-theme-argon
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+#允许ROOT编译
+export FORCE_UNSAFE_CONFIGURE=1
+
+#报错修复
+
+cp /usr/bin/upx /workdir/openwrt/staging_dir/host/bin/
+cp /usr/bin/upx-ucl /workdir/openwrt/staging_dir/host/bin/
+
+ls
+#修改后台IP
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+#修改默认主题(lede暂不需要)
+#cd ./package/
+#git clone https://github.com/jerrykuku/luci-theme-argon.git
+#cd ../
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
