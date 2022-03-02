91ebeba191a05ed39a9b0886467a4c12b2320a14
guaotiantang <35430449+guaotiantang@users.noreply.github.com>
2022-03-02 12:24:50 +0800

    Update make_befor.sh

--- XIAOMI-4A_100M/make_befor.sh
+++ XIAOMI-4A_100M/make_befor.sh
@@ -12,8 +12,8 @@ ls
 #修改后台IP
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 #修改默认主题(lede暂不需要)
-#cd ./package/
-#git clone https://github.com/jerrykuku/luci-theme-argon.git
-#cd ../
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+cd ./package/
+git clone https://github.com/jerrykuku/luci-theme-argon.git
+cd ../
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
