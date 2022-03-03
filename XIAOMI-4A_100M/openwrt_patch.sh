e4f66355a6e33174a6f4a9dae34127c0ac9dd00a
guaotiantang <35430449+guaotiantang@users.noreply.github.com>
2022-03-03 11:52:40 +0800

    Update openwrt_patch.sh

--- XIAOMI-4A_100M/openwrt_patch.sh
+++ XIAOMI-4A_100M/openwrt_patch.sh
@@ -1,47 +1,36 @@
 #!/bin/bash
-
 rm -f target/linux/ramips/dts/mt7628an_xiaomi_mir4a-100m.dts
 cat >> target/linux/ramips/dts/mt7628an_xiaomi_mir4a-100m.dts << EOF
 //SPDX-License-Identifier: GPL-2.0-or-later OR MIT
 /dts-v1/;
-
 #include "mt7628an.dtsi"
-
 #include <dt-bindings/gpio/gpio.h>
 #include <dt-bindings/input/input.h>
-
 / {
 	compatible = "xiaomi,mir4a-100m", "mediatek,mt7628an-soc";
 	model = "Xiaomi Mi Router 4A (100M Edition)";
-
 	chosen {
 		bootargs = "console=ttyS0,115200";
 	};
-
 	aliases {
 		led-boot = &power_yellow;
 		led-failsafe = &power_yellow;
 		led-running = &power_blue;
 		led-upgrade = &power_yellow;
 	};
-
 	leds {
 		compatible = "gpio-leds";
-
 		power_blue: power_blue {
 			label = "mir4a-100m:blue:power";
 			gpios = <&gpio0 11 GPIO_ACTIVE_LOW>;
 		};
-
 		power_yellow: power_yellow {
 			label = "mir4a-100m:yellow:power";
 			gpios = <&gpio1 12 GPIO_ACTIVE_LOW>;
 		};
 	};
-
 	keys {
 		compatible = "gpio-keys";
-
 		reset {
 			label = "reset";
 			gpios = <&gpio1 6 GPIO_ACTIVE_LOW>;
@@ -49,38 +38,31 @@ cat >> target/linux/ramips/dts/mt7628an_xiaomi_mir4a-100m.dts << EOF
 		};
 	};
 };
-
 &spi0 {
 	status = "okay";
-
 	flash@0 {
 		compatible = "jedec,spi-nor";
 		reg = <0>;
 		spi-max-frequency = <10000000>;
-
 		partitions {
 			compatible = "fixed-partitions";
 			#address-cells = <1>;
 			#size-cells = <1>;
-
 			partition@0 {
 				label = "u-boot";
 				reg = <0x0 0x30000>;
 				read-only;
 			};
-
 			partition@30000 {
 				label = "u-boot-env";
 				reg = <0x30000 0x10000>;
 				read-only;
 			};
-
 			factory: partition@40000 {
 				label = "factory";
 				reg = <0x40000 0x10000>;
 				read-only;
 			};
-
 			partition@50000 {
 				label = "firmware";
 				reg = <0x50000 0xfb0000>;
@@ -89,11 +71,9 @@ cat >> target/linux/ramips/dts/mt7628an_xiaomi_mir4a-100m.dts << EOF
 		};
 	};
 };
-
 &pcie {
 	status = "okay";
 };
-
 &pcie0 {
 	wifi@0,0 {
 		compatible = "mediatek,mt76";
@@ -102,28 +82,23 @@ cat >> target/linux/ramips/dts/mt7628an_xiaomi_mir4a-100m.dts << EOF
 		ieee80211-freq-limit = <5000000 6000000>;
 	};
 };
-
-&ethernet {
-	nvmem-cells = <&macaddr_factory_4>;
-	nvmem-cell-names = "mac-address";
-	mac-address-increment = <(-1)>;
-};
-
-&esw {
-	mediatek,portmap = <0x3e>;
-	mediatek,portdisable = <0x2a>;
-};
-
-&factory {
-	compatible = "nvmem-cells";
-	#address-cells = <1>;
-	#size-cells = <1>;
-
-	macaddr_factory_4: macaddr@4 {
-		reg = <0x4 0x6>;
+&state_default {
+	gpio {
+		ralink,group = "gpio", "wdt", "wled_an";
+		ralink,function = "gpio";
 	};
 };
-
+&ethernet {
+	mtd-mac-address = <&factory 0x4>;
+	mtd-mac-address-increment = <(-1)>;
+};
+&esw {
+	mediatek,portmap = <0x2f>;
+	mediatek,portdisable = <0x2a>;
+};
+&wmac {
+	status = "okay";
+};
 
 EOF
 
