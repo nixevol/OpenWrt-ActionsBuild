d100e0020d1d9efb935022b14ac270e9ef0959f1
guaotiantang <35430449+guaotiantang@users.noreply.github.com>
2022-04-22 00:07:04 +0800

    Update make_befor.sh

--- P2W_R619AC-128M/make_befor.sh
+++ P2W_R619AC-128M/make_befor.sh
@@ -15,11 +15,11 @@ sed -i 's/OpenWrt/R619ac/g' package/base-files/files/bin/config_generate
 #修改后台IP
 sed -i 's/192.168.1.1/192.168.32.1/g' package/base-files/files/bin/config_generate
 #修改主题
-rm -rf package/lean/luci-theme-argon
-
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
-git clone https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge
-
-
+if [[ ! -d "$testPath" ]]; then
+ rm -rf package/lean/luci-theme-argon
+ git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
+ git clone https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge
 sed -i "s/OpenWrt /NIXEVOL build $(TZ=UTC-8 date "+%Y.%m.%d")@ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+fi
+
 
